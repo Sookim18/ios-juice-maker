4a91545080fa1a20a697a7c9a373dda1a568a61f
Sookim18 <scstnghks@naver.com>
2021-03-08 13:54:54 +0900

    과일과 쥬스타입 열거형으로 지정

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -1,9 +1,27 @@
 //
 //  JuiceMaker - JuiceMaker.swift
-//  Created by yagom. 
+//  Created by sookim. 
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import Foundation
 
-/// 쥬스 메이커 타입 
+// 과일 타입 지정
+enum FruitsType {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
+
+// 쥬스 타입 지정
+enum JuiceType {
+    case strawberryJuice
+    case bananaJuice
+    case kiwiJuice
+    case pineappleJuice
+    case strawberryBananaJuice
+    case mangoJuice
+    case mangoKiwiJuice
+}
